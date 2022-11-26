e425ec435fe28e7ff13db904311c1d3895be3438
jenny han <jlhan@stanford.edu>
2022-11-26 13:23:28 -0800

    working with a redirect

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-anonymous-default-export */
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from "../../db/client"
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // res.status(200).json({ name: 'John Doe' })
+  const slug = req.query["slug"];
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: "no slug specified!" }))
+    return;
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug
+      }
+    }
+  })
+
+  if (!data) {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: "no slug found!" }))
+    return;
+  }
+  return res.redirect(data.url);
+}
